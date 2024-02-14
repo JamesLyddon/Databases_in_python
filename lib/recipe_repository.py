b4f63532fe4c33713887817b67c4d5c3e8e43f88
JamesLyddon <jameslyddon@gmail.com>
2024-02-14 17:16:27 +0000

    added find method to RecipeRepository class

--- lib/recipe_repository.py
+++ lib/recipe_repository.py
@@ -11,3 +11,8 @@ class RecipeRepository:
             item = Recipe(row["id"], row["name"], row["avg_cooking_time"], row["rating"])
             recipes.append(item)
         return recipes
+    
+    def find(self, id):
+        rows = self._connection.execute('SELECT * FROM recipes WHERE id = %s', [id])
+        row = rows[0]
+        return Recipe(row["id"], row["name"], row["avg_cooking_time"], row["rating"])
--- tests/test_recipe_repository.py
+++ tests/test_recipe_repository.py
@@ -17,3 +17,15 @@ def test_get_all_recipes(db_connection):
         Recipe(2, 'Sandwich', 2, 3),
         Recipe(3, 'Soup', 15, 2)
     ]
+
+"""
+When we call RecipeRepository.find(id)
+We get the Recipe object with that id
+"""
+
+def test_find_by_id(db_connection):
+    db_connection.seed("seeds/recipes_seed.sql")
+    repo = RecipeRepository(db_connection)
+
+    recipe = repo.find(2)
+    assert recipe == Recipe(2, 'Sandwich', 2, 3)
